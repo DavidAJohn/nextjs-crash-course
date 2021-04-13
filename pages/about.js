8ee0f2bafcc9da976bce751dcb2db3997ba6107c
David John <davidajohn@gmail.com>
2021-04-13 13:56:56 +0100

    Added a custom meta data component

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Meta from '../components/Meta';
+
 const about = () => {
     return (
         <div>
+            <Meta title='WebDev Newz - About Us' />
             <h1>About</h1>
         </div>
     )
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,12 +1,10 @@
 import Link from 'next/link';
 import { server } from '../../../config';
-import { useRouter } from 'next/router';
+import Meta from '../../../components/Meta';
 
 const article = ({article}) => {
-    // const router = useRouter();
-    // const id = router.query.id;
-
     return <>
+        <Meta title={`WebDev Newz - ${article.title}`} description={article.excerpt} />
         <h1>{article.title}</h1>
         <p>{article.body}</p>
         <br />
